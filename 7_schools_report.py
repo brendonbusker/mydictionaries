c19a398b5ef0851f045fe6e68c2466278ab40032
Brendon <brendon_busker1@baylor.edu>
2023-02-06 18:20:37 -0600

    complete school report from home pc

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -11,9 +11,49 @@ Copy that info here:
 "NCAA/NAIA conference number football (IC2020)","130","Southeastern Conference"
 
 
-Display report for all universities that have a graduation rate for Women over 50%
+Display report for all universities that have a graduation rate for Women over 75%
 Display report for all universities that have a total price for in-state students living off campus over $50,000
 
 
 
 """
+
+import json
+
+infile = open('school_data.json', 'r')
+
+schools = json.load(infile)
+conference_schools = [372, 108, 107, 130]
+schools_women_grad_rate = []
+schools_off_campus = []
+
+#print(type(schools))
+
+#How many schools are in this file?
+#print(len(schools))
+
+
+for i in range(0, len(schools)):
+    school_dict = schools[i]
+
+    #Determines if correct conference
+    if school_dict["NCAA"]["NAIA conference number football (IC2020)"] in conference_schools:
+        #Determines if correct grad rate for women then appends to list
+        if school_dict["Graduation rate  women (DRVGR2020)"] > 75:
+            schools_women_grad_rate.append(school_dict["instnm"])
+
+        #Determines if total price is greater than 50,000
+        if type(school_dict["Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"]) == int:
+            if school_dict["Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"] > 50000:
+                schools_off_campus.append(school_dict["instnm"])
+        
+
+print("Schools With Women Grad rate above 75% : ", "\n", "-" * 40, sep = "")
+for i in range(0, len(schools_women_grad_rate)):
+    print(schools_women_grad_rate[i])
+
+print("\nSchools with a total price for in-state students living off campus over $50,000 : ", "\n", "-" * 80, sep = "")
+for i in range(0, len(schools_off_campus)):
+    print(schools_off_campus[i])
+
+
